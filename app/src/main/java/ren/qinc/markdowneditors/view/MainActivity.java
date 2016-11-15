460428472cd7a6150bc21ec4cac0b9be6ee67991
EdenLee <tlljiao@163.com>
2016-11-15 23:47:05 +0800

    主分支添加一些无用打印

--- app/src/main/java/ren/qinc/markdowneditors/view/MainActivity.java
+++ app/src/main/java/ren/qinc/markdowneditors/view/MainActivity.java
@@ -77,6 +77,7 @@ public class MainActivity extends BaseDrawerLayoutActivity {
 
     @Override
     public void initData() {
+        Log.d("MainActivity", "has no log! just test add one");
     }
 
     @Override
